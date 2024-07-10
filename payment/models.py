d604bae62eaafa1a2afc55af06511001898196c8
leonid Petrov <leonidpetrov637@gmail.com>
2024-07-10 14:25:06 +0300

    add models payment

---
+++ payment/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from config.settings import NULLABLE
+from course.models import Course
+from lesson.models import Lesson
+from users.models import User
+
+
+class Payment(models.Model):
+    users = models.ManyToManyField(
+        User,
+        on_delete=models.SET_NULL(),
+        verbose_name='Пользователь',
+        help_text='Укажите пользователя',
+        **NULLABLE
+    )
+    date_payment = models.DateField(
+        auto_now_add=True,
+    )
+    paid_course = models.ManyToManyField(
+        Course,
+        on_delete=models.SET_NULL(),
+        verbose_name='Курс',
+        help_text='укажите курс',
+        **NULLABLE
+    )
+    paid_lesson = models.ManyToManyField(
+        Lesson,
+        on_delete=models.SET_NULL(),
+        verbose_name='Урок',
+        help_text='Укажите урок',
+        **NULLABLE
+    )
+    sum = models.PositiveIntegerField(
+        verbose_name='Сумма',
+        help_text='Укажите сумму',
+        **NULLABLE
+    )
+    cash_payment = models.BooleanField(
+        verbose_name='Наличные',
+        help_text='Выберите наличные',
+        **NULLABLE
+    )
+    payment_to_account = models.BooleanField(
+        verbose_name='Перевод',
+        help_text='Выберите перевод на счет',
+        **NULLABLE
+    )
