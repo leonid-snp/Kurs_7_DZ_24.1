ad396be8a9d883caf543118fe3a03ffe4b658041
leonid Petrov <leonidpetrov637@gmail.com>
2024-07-11 13:39:17 +0300

    implemented the output of all lessons in this course

--- course/serializer.py
+++ course/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from course.models import Course
 from lesson.models import Lesson
+from lesson.serializer import LessonSerializer
 
 
 class CourseSerializer(ModelSerializer):
@@ -11,10 +12,14 @@ class CourseSerializer(ModelSerializer):
 
 class CourseDetailSerializer(ModelSerializer):
     number_of_lessons = SerializerMethodField()
+    lesson = SerializerMethodField()
 
     def get_number_of_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
 
+    def get_lesson(self, course):
+        return [lesson.name for lesson in Lesson.objects.filter(course=course)]
+
     class Meta:
         model = Course
-        fields = ('name', 'description', 'number_of_lessons')
+        fields = ('name', 'description', 'number_of_lessons', 'lesson')
